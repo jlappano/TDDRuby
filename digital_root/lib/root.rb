10170ab4b02f66ac3595feeb43aff3fbfe1a74cb
Julie lappano <julie.lappano@gmail.com>
2014-01-30 15:05:50 -0500

    schmee

--- digital_root/lib/root.rb
+++ digital_root/lib/root.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
+## `digital_root`
+
+# Write a method, `digital_root(num)`. It should sum the digits of a
+# positive integer. If it is greater than 10, sum the digits of the
+# resulting number. Keep repeating until there is only one digit in the
+# result, called the "digital root". Do not use string conversion within
+# your method.
+
 def digital_root(num)
   reductions = []
 
@@ -18,5 +26,3 @@ def digital_root(num)
   end
 
 end
-
-digital_root(59)
