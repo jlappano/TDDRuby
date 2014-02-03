706ed3eb2d9e93e4c343242b95d4e337494d7a6a
Julie lappano <julie.lappano@gmail.com>
2014-02-03 15:56:12 -0500

    folding directories

---
+++ folding_cipher/lib/folding_cipher.rb
@@ -0,0 +1,2 @@
+# Implement the Folding Cipher. It folds the alphabet in half and uses
+# the adjacent letter. Ie. a <=> z, b <=> y, c <=> x, m <=> n.
---
+++ folding_cipher/spec/folding_cipher_spec.rb
@@ -0,0 +1,4 @@
+# Implement the Folding Cipher. It folds the alphabet in half and uses
+# the adjacent letter. Ie. a <=> z, b <=> y, c <=> x, m <=> n.
+require 'spec_helper'
+require_relative '../lib/folding_cipher'
