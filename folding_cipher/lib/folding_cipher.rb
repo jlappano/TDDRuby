da277640c64746ceebacf78b28fbfc945ccb5d8c
Julie lappano <julie.lappano@gmail.com>
2014-02-03 16:42:17 -0500

    passing

--- folding_cipher/lib/folding_cipher.rb
+++ folding_cipher/lib/folding_cipher.rb
@@ -1,2 +1,12 @@
 # Implement the Folding Cipher. It folds the alphabet in half and uses
 # the adjacent letter. Ie. a <=> z, b <=> y, c <=> x, m <=> n.
+
+
+def folding_cipher(string)
+  a = ("a".."z").to_a
+  b = ("a".."z").to_a.reverse
+  stone = Hash[a.zip(b)]
+
+  string.each_char {|char| string[char] = stone[char]}
+
+end
--- folding_cipher/spec/folding_cipher_spec.rb
+++ folding_cipher/spec/folding_cipher_spec.rb
@@ -2,3 +2,11 @@
 # the adjacent letter. Ie. a <=> z, b <=> y, c <=> x, m <=> n.
 require 'spec_helper'
 require_relative '../lib/folding_cipher'
+
+
+describe "#folding_cipher" do
+  it "returns an encryption" do
+    expect(folding_cipher("aabbcc")).to eq("zzyyxx")
+    expect(folding_cipher("mlkj")).to eq("nopq")
+  end
+end
